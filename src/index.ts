76d6a3d3562e9eaef576d78f4ab7a5defe2afa04
Mohammad Bilal <mdbilal7620@gmail.com>
2025-02-17 01:02:36 +0530

    secrets.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import { PORT } from '../schema/secrets';
 
 const app = express()
 
@@ -6,6 +7,6 @@ app.get('/', (req, res)=>{
    res.send("Working Server...") ;
 });
 
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
     console.log("SERVER ON PORT 5000");
 });
